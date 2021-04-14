c43f457db45aa584a39ebc74da82633d8546c91c
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2021-04-14 16:48:48 +0300

    feat: add getAttributes function

--- utils.js
+++ utils.js
@@ -21,6 +21,14 @@ function createElement(type, content, attributes) {
   return element;
 }
 
+function getAttributes(select, input) {
+  const attributes = {};
+  if (elementsExist(select, input) && select.value && input.value) {
+    Object.assign(attributes, { [select.value]: input.value });
+  }
+  return attributes;
+}
+
 function capitalize(str) {
   return str.slice(0, 1).toUpperCase() + str.slice(1).toLowerCase();
 }
@@ -29,4 +37,4 @@ function elementsExist(...elements) {
   return elements.every((element) => element != null);
 }
 
-export { createElement, capitalize, elementsExist };
+export { createElement, getAttributes, capitalize, elementsExist };
