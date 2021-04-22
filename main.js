5b828d4cdd4d58d704944bc845177971ea0dd4fd
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2021-04-22 20:41:29 +0300

    Update main.js

--- main.js
+++ main.js
@@ -87,44 +87,52 @@ const actions = {
     if (graphElements.length === 0) {
       graphElements.push(currentElement);
     } else {
-      currentElement.parentElement = graphElements[graphDomElements.length - 1];
-      const parentElement = graphElements.find(
-        (x) => x.element === currentElement.parentElement
-      );
-      parentElement.children.push(currentElement);
+      const innermostElement = checkForChildren(graphElements[0]);
+      currentElement.parentElement = innermostElement;
+      innermostElement.children.push(currentElement);
     }
-    console.log(graphElements);
-    const graphDomElements = graphElements.map((element) =>
-      createDiv(element.element, { className: "graph-element" })
+    const graphDomElements = graphElements.map((x) =>
+      generateElement(
+        htmlFactory,
+        `create${capitalize(x.element)}`,
+        x.attributes,
+        x.value
+      )
     );
-    const graphDomElementsWrapper = createDiv(graphDomElements, {
-      id: "graph-elements-wrapper",
-      className: "graph-elements-wrapper",
-    });
-    const graphDomElementsChildren = [...graphDomElementsWrapper.children];
-    const lastGraphChild =
-      graphDomElementsChildren[graphDomElementsChildren.length - 1];
-    graphDomElementsChildren.forEach((child, index) => {
-      child.style.marginLeft = `${index * 20}px`;
-    });
-    if (domReferences.elementContent().value) {
-      lastGraphChild.textContent = domReferences.elementContent().value;
+    console.log(graphElements);
+    console.log(graphDomElements);
+    if (graphElements.length > 0) {
+      console.log(getChildren(graphElements[0]));
     }
-    domReferences.generatedElementsGraph().innerHTML = "";
-    domReferences.generatedElementsGraph().appendChild(graphDomElementsWrapper);
+    resetFields(
+      domReferences.selectElements(),
+      domReferences.elementAttributes(),
+      domReferences.elementAttributesInput(),
+      domReferences.elementContent()
+    );
+    // const graphDomElements = graphElements.map((element) =>
+    //   createDiv(element.element, { className: "graph-element" })
+    // );
+    // const graphDomElementsWrapper = createDiv(graphDomElements, {
+    //   id: "graph-elements-wrapper",
+    //   className: "graph-elements-wrapper",
+    // });
+    // const graphDomElementsChildren = [...graphDomElementsWrapper.children];
+    // const lastGraphChild =
+    //   graphDomElementsChildren[graphDomElementsChildren.length - 1];
+    // graphDomElementsChildren.forEach((child, index) => {
+    //   child.style.marginLeft = `${index * 20}px`;
+    // });
+    // if (domReferences.elementContent().value) {
+    //   lastGraphChild.textContent = domReferences.elementContent().value;
+    // }
+    // domReferences.generatedElementsGraph().innerHTML = "";
+    // domReferences.generatedElementsGraph().appendChild(graphDomElementsWrapper);
   },
   removeElement: (e) => {
     console.log(e.target);
   },
   outputElement: () => {
-    const elements = graphElements.map((element) =>
-      generateElement(
-        htmlFactory,
-        `create${capitalize(element)}`,
-        "attributes",
-        "value"
-      )
-    );
     const element = generateElement(
       htmlFactory,
       `create${capitalize(domReferences.selectElements().value)}`,
@@ -155,6 +163,24 @@ function checkForChildren(element) {
   return checkForChildren(element.children[0]);
 }
 
+function getChildren(element, domElements = []) {
+  if (element.children.length === 0) return element;
+  element.children.forEach((child) => {
+    if (child.children.length > 0) {
+      getChildren(child, domElements);
+    }
+    domElements.push(child);
+  });
+  return domElements;
+}
+
+function resetFields(...fields) {
+  fields.forEach((field) => {
+    field.value = "";
+  });
+  return "Reset fields";
+}
+
 function clickHandler(e) {
   if (!e.target.dataset.id) return;
   const dataId = transformToCamelCase(e.target.dataset.id);
@@ -164,37 +190,3 @@ function clickHandler(e) {
 }
 
 document.addEventListener("click", clickHandler);
-
-//  buildElement: () => {
-//    const elementFields = domReferences.elementFields();
-//    const lastElementFields =
-//      elementFields.length > 1
-//        ? elementFields[elementFields.length - 1]
-//        : elementFields;
-//    const selectElements = lastElementFields.querySelector(".select-elements");
-//    const elementContent = lastElementFields.querySelector(".element-content");
-//    const elementAttributes = lastElementFields.querySelector(
-//      ".element-attributes"
-//    );
-//    const elementAttributesInput = lastElementFields.querySelector(
-//      ".element-attributes-input"
-//    );
-
-//    const attributes = getAttributes(elementAttributes, elementAttributesInput);
-//    const element = generateElement(
-//      htmlFactory,
-//      `create${capitalize(selectElements.value)}`,
-//      attributes,
-//      elementContent.value
-//    );
-
-//    if (!parentElement) {
-//      parentElement = element;
-//    } else {
-//      let currentParentElement = parentElement;
-//      currentParentElement = checkForChildren(parentElement);
-//      currentParentElement.appendChild(element);
-//    }
-
-//    domReferences.generatedElementsGraph().appendChild(parentElement);
-//  };
