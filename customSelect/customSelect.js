507ec7fd477a65c2a7294ac08e7f249eb6cb3485
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2021-04-30 22:06:19 +0300

    extend functionality

--- customSelect/customSelect.js
+++ customSelect/customSelect.js
@@ -1,23 +1,65 @@
 import htmlFactory from "../htmlFactory.js";
 const { createDiv, createLabel, createUl, createLi } = htmlFactory;
 
-class Select {
-  constructor(element) {
-    this.element = element;
-    this.customElement = createDiv("", {
-      className: "custom-element-container",
+const ACTIVE_CLASS = "active";
+const SHOW_CLASS = "show";
+
+function createCustomSelectElement(element) {
+  element.style.display = "none";
+  const options = [...element.querySelectorAll("option")].map((option) =>
+    createLi(option.textContent, { className: "custom-element-list-item" })
+  );
+  const customElementLabel = createLabel("Select element", {
+    className: "custom-element-label",
+  });
+  const customElementList = createUl(options, {
+    className: "custom-element-list",
+  });
+  const customElement = createDiv([customElementLabel, customElementList], {
+    className: "custom-element-container",
+  });
+  customElementLabel.addEventListener("click", () => {
+    customElementList.classList.toggle(SHOW_CLASS);
+  });
+  customElementList.addEventListener("click", (e) => {
+    options.forEach((option) => {
+      option.classList.remove(ACTIVE_CLASS);
     });
-    this.customElementLabel = createLabel("Select element", {
-      className: "custom-element-label",
-    });
-    this.customElementList = createUl("", { className: "custom-element-list" });
-    this.options = element
-      .querySelectorAll("option")
-      .map((option) =>
-        createLi(option.value, { className: "custom-element-list-item" })
-      );
-    element.after(this.customElement);
+    if (e.target.nodeName.toLowerCase() === "li") {
+      e.target.classList.add(ACTIVE_CLASS);
+      customElementLabel.textContent = e.target.textContent;
+    }
+  });
+  customElement.tabIndex = 0;
+  customElement.addEventListener("keydown", (e) => {
+    const activeElement = customElement.querySelector(`.${ACTIVE_CLASS}`);
+    if (e.key === "Enter" || e.key === "Escape") {
+      customElementList.classList.remove(SHOW_CLASS);
+    } else if (e.key === " ") {
+      customElementList.classList.toggle(SHOW_CLASS);
+    }
+    return changeActiveElement(e.key, activeElement, customElementLabel);
+  });
+  element.after(customElement);
+  return customElement;
+}
+
+function changeActiveElement(eventKey, element, label) {
+  const elementToChange = getElementToChange(eventKey, element);
+  if (elementToChange == null) return;
+  element.classList.remove(ACTIVE_CLASS);
+  elementToChange.classList.add(ACTIVE_CLASS);
+  elementToChange.scrollIntoView({ block: "nearest" });
+  label.textContent = elementToChange.textContent;
+}
+
+function getElementToChange(key, element) {
+  if (element == null) return;
+  if (key === "ArrowUp") {
+    return element.previousElementSibling;
+  } else if (key === "ArrowDown") {
+    return element.nextElementSibling;
   }
 }
 
-export default Select;
+export default createCustomSelectElement;
